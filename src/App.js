310d10f01db8c7601f34475fcb84fac2556a4a4a
Parth Patel <parth8733@gmail.com>
2023-08-19 16:32:52 -0400

    Implemented Search Functionality

--- src/App.js
+++ src/App.js
@@ -6,36 +6,55 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: { firstName: "Parthiiiii", lastName: "Patel" },
-      company: "ZTM",
+      monsters: [],
+      searchFeild: "",
     };
+    console.log("CONSTRUCTOR");
   }
+  componentDidMount() {
+    console.log("COMPONENT DID MOUNT");
+    fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
+      response.json().then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log("resut", this.state);
+          }
+        )
+      )
+    );
+  }
+  onSearchChange = (event) => {
+    const searchFeild = event.target.value.toLowerCase();
+    this.setState(() => {
+      return { searchFeild };
+    });
+  };
   render() {
+    console.log("RENDER");
+
+    const { monsters, searchFeild } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonster = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchFeild);
+    });
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at{" "}
-            {this.state.company}
-          </p>
-          <button
-            onClick={() => {
-              this.setState(
-                () => {
-                  return {
-                    name: { firstName: "parth", lastName: "patel" },
-                  };
-                },
-                () => {
-                  console.log(this.state.name);
-                }
-              );
-            }}
-          >
-            Change Name
-          </button>
-        </header>
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={onSearchChange}
+        />
+        {filteredMonster.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
